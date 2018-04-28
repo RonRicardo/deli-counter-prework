8e918d0a3c1404b94773a6885e66922fadad1be2
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-28 01:10:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,10 @@ def line(list)
   if list.size === 0
     puts "The line is currently empty."
   else
-    index = 1
-    customer_list = list.each {
-      |name| print "#{index}. #{name}"
-    }
-    puts "The line is currently: ".concat(customer_list)
+    current_line = "The line is currently:"
+    list.each_with_index do |per, index|
+      current_line += " #{index + 1}. #{per}"
+    end
+    current_line
+  end
 end
