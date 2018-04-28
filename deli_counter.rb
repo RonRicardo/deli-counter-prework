19b5f999fac9b44a23211dba52c6204f799fcf1f
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-28 01:13:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,6 +14,6 @@ def take_a_number(line, name)
   if line.size === 0
     puts "Welcome, #{name}. You are number 1 in line."
   else
-    puts "Welcome, #{name}. You are number #{line.size} in line."
+    puts "Welcome, #{name}. You are number #{line.size + 1} in line."
   end
 end
